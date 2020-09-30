210d0c05c2b0319a5c3ca6154ffc51f05049591d
Caivan <caivan2@gmail.com>
2020-09-29 21:48:19 -0500

    Project creation

---
+++ src/main/java/com/devbeerloper/apirestexample/ApiRestExampleApplication.java
@@ -0,0 +1,13 @@
+package com.devbeerloper.apirestexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devbeerloper/apirestexample/ApiRestExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devbeerloper.apirestexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
